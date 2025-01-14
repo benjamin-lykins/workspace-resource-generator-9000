67c114bfe5830398ed29d02c31927c524c2f72b1
Benjamin Lykins <benjamin.m.lykins@gmail.com>
2025-01-14 15:20:26 -0500

    remove unnecessary allow_destroy variable from workspace module

--- main.tf
+++ main.tf
@@ -45,8 +45,7 @@ module "workspacer" {
   organization      = var.tfe_organization
   working_directory = "./workspace-random"
   auto_apply        = true
-  allow_destroy     = true
-
+  
   run_trigger_source_workspaces = ["workspace-resource-generator-9000"]
   run_trigger_auto_apply        = true
 
