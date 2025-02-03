d5504e7412ff9cae351997bc54d8d377931c2b98
Benjamin Lykins <benjamin.m.lykins@gmail.com>
2025-02-03 13:59:52 -0500

    remove README.md and workspace-random module; refactor main.tf to use random provider and simplify resource creation

--- README.md
+++
@@ -1,5 +0,0 @@
-# workspace-resource-generator-9000
-
-Literally used to create random resources in a workspace. 
-
-Was being used to create workspaces with 'artificial' resources, in order to test AWS replication when performing cross region failover in AWS with TFE. 
--- main.tf
+++ main.tf
@@ -1,76 +1,22 @@
 terraform {
   required_providers {
-    tfe = {
-      source  = "hashicorp/tfe"
-      version = "~> 0.0"
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
 
-provider "tfe" {
-  hostname     = var.tfe_hostname
-  organization = var.tfe_organization
+provider "random" {}
+
+
+variable "resource_count" {
+  type    = number
+  default = 5
+
 }
 
-variable "tfe_hostname" {
-  description = "The hostname of the Terraform Enterprise instance"
-  type        = string
-}
+resource "random_pet" "this" {
+  count = var.resource_count
 
-variable "tfe_organization" {
-  description = "The organization to create the workspaces in."
-  type        = string
-}
-
-variable "downstream_workspaces" {
-  description = "The number of workspaces to create."
-  type        = number
-  default     = 5
-}
-
-module "upstream" {
-  source = "github.com/benjamin-lykins/terraform-tfe-workspacer.git"
-
-  workspace_name    = "upstream-ws"
-  organization      = var.tfe_organization
-  working_directory = "./workspace-random"
-  auto_apply        = true
-  force_delete = true
-
-  tfvars = {
-    resource_count = 0
-  }
-
-  vcs_repo = {
-    identifier     = "benjamin-lykins/workspace-resource-generator-9000"
-    branch         = "main"
-    oauth_token_id = "ot-hcFMM5ZhpaNrj4TH"
-  }
-}
-
-module "downstream" {
-  source = "github.com/benjamin-lykins/terraform-tfe-workspacer.git"
-  
-  count             = var.downstream_workspaces
-
-  workspace_name    = "downstream-${count.index}"
-  organization      = var.tfe_organization
-  working_directory = "./workspace-random"
-  auto_apply        = true
-  force_delete = true
-
-  run_trigger_source_workspaces = ["upstream-ws"]
-  run_trigger_auto_apply        = true
-
-  tfvars = {
-    resource_count = 5
-  }
-
-  vcs_repo = {
-    identifier     = "benjamin-lykins/workspace-resource-generator-9000"
-    branch         = "main"
-    oauth_token_id = "ot-hcFMM5ZhpaNrj4TH"
-  }
-  
-  depends_on = [ module.upstream ]
 }
